4265a886b49204c788cba28b3ece3438ca7ddbf8
Arthur <arthur155188@gmail.com>
2026-01-30 22:58:08 +0500

    Добавил возможность ввода count

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from scraping.base import ArticlesScraping
 
 if __name__ == '__main__':
     words = input("Enter words: ").split()
+    count = int(input("Enter count: "))
     Article = ArticlesScraping(words)
-    result = Article.collect_articles(count=5)
+    result = Article.collect_articles(count)
     print(json.dumps(result, ensure_ascii=False, indent=2))
